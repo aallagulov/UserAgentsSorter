5119ce9bda1d63e4632e21a7f102e847a377253c
Anvar Allagulov <a.allagulov@corp.mail.ru>
2022-07-18 01:18:37 +0300

    compiling but works incorrectly

--- ua_sort.go
+++ ua_sort.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"container/heap"
 	"encoding/csv"
 	"fmt"
 	"io"
@@ -17,22 +18,23 @@ const (
 type UAStatRecord struct {
 	UserAgent  string
 	TimesSeen  int64
-	LastSeen   time.Time
 	LastSeenTS int64
 	value      int64
 }
 
 type UAStatHeap []UAStatRecord
 
-// // Len, Less, Swap для реализации интерфейса sort.Interface
-// func (h UAStatHeap) Len() int           { return len(h) }
-// func (h UAStatHeap) Less(i, j int) bool { return h[i] < h[j] }
-// func (h UAStatHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+// Len, Less, Swap для реализации интерфейса sort.Interface
+func (h UAStatHeap) Len() int           { return len(h) }
+func (h UAStatHeap) Less(i, j int) bool { return h[i].value < h[j].value }
+func (h UAStatHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
 
-func (h *UAStatHeap) Push(line []string) {
+func (h *UAStatHeap) Push(linei interface{}) {
+	line := linei.([]string)
+
 	var rec UAStatRecord
 
-	rec.UserAgent = line[1]
+	// rec.UserAgent = line[1]
 
 	intField, err := strconv.ParseInt(line[2], 10, 64)
 	if err != nil {
@@ -41,11 +43,9 @@ func (h *UAStatHeap) Push(line []string) {
 	rec.TimesSeen = intField
 
 	time, err := time.Parse(layout, line[37])
-	// intField, err := strconv.ParseInt(field, 10, 64)
 	if err != nil {
 		log.Fatal(err)
 	}
-	rec.LastSeen = time
 	rec.LastSeenTS = time.Unix()
 
 	rec.value = rec.TimesSeen*10000000000 + rec.LastSeenTS
@@ -53,13 +53,13 @@ func (h *UAStatHeap) Push(line []string) {
 	*h = append(*h, rec)
 }
 
-// func (h *UAStatHeap) Pop() interface{} {
-// 	old := *h
-// 	n := len(old)
-// 	x := old[n-1]
-// 	*h = old[0 : n-1]
-// 	return x
-// }
+func (h *UAStatHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
 
 func main() {
 	f, err := os.Open("data/whatismybrowser-user-agent-database.csv")
@@ -76,9 +76,9 @@ func main() {
 	}
 
 	h := &UAStatHeap{}
-	// heap.Init(h)
+	heap.Init(h)
 
-	for i := 0; i < 2; i++ {
+	for i := 0; i < 5; i++ {
 		line, err := csvReader.Read()
 		if err == io.EOF {
 			break
@@ -89,9 +89,11 @@ func main() {
 		}
 
 		fmt.Print(line[2], "\t", line[37], "\t", line[1], "\n")
-		// heap.Push(h, line)
-		h.Push(line)
+		heap.Push(h, line)
 	}
 
-	fmt.Print(h, "\n")
+	l := h.Len()
+	for i := 0; i < l; i++ {
+		fmt.Print(h.Pop(), "\n")
+	}
 }
